c2bcd670214c8e49afad54ee3df350b9ae7789bb
Claire COMMISSAIRE <claire.commissaire@sciencespo.fr>
2020-12-10 15:26:06 +0100

    Everything ok, corrected a leak by freeing stock before putting it to NULL instead of the contrary, remains headers

--- get_next_line.h
+++ get_next_line.h
@@ -1,7 +1,13 @@
 
-#include <unistd.h>
-#include <stdlib.h>
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
 
+size_t	ft_strlen(const char *s);
+int		get_next_line(int fd, char **line);
+char	*ft_fill_line(char *stock, int i, char *line);
+char	*ft_strconcat(char *s1, char *s2, int size);
+char	*ft_newstock(char **stock, int i);
 
-int get_next_line(int fd, char **line);
-void	*ft_memset(void *s, int c, size_t n);
+#endif
